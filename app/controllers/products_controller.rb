41242985b67643d53c255dffec22f77f4a4e4cd7
sclarke5 <sclarke5555@gmail.com>
2018-08-24 14:22:03 -0400

    show and edit pages work, partial view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,15 +9,35 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
+    if @product.save
+      redirect_to product_url(@product.id)
+    else
+      render 'products/new'
+    end
   end
 
   def update
+    @product = Product.find(params[:id])
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
+    if @product.save
+      redirect_to product_url(@product.id)
+    else
+      render "products/edit"
+    end
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def destroy
