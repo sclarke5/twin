ba30f2977a48a05c730cee3b5259fd4226a15bc6
sclarke5 <sclarke5555@gmail.com>
2018-09-18 18:33:40 -0400

    submit review form, displays on product show

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,16 @@
 class ReviewsController < ApplicationController
 
   def create
+    @review = Review.new
+    @review.comment = params[:review][:comment]
+    @review.product_id = params[:product_id]
+
+    if @review.save
+			redirect_to product_path(id: params[:product_id])
+		else
+			flash[:warning] = @review.errors.full_messages
+		 	redirect_to product_path(id: params[:product_id])
+		end
   end
 
   def edit
