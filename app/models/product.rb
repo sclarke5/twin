cb2736de72a6aed885318b3d23b77cf7dd9ebaae
sclarke5 <sclarke5555@gmail.com>
2018-09-18 15:18:49 -0400

    review model created, working association with products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,11 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :price_in_cents, presence: true
+
+  has_many :reviews 
+
+  def price_in_dollars
+    price_in_dollars = price_in_cents.to_f / 100
+    sprintf("$%.2f", price_in_dollars)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_23_220814) do
+ActiveRecord::Schema.define(version: 2018_09_18_190234) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "price_in_cents"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "comment"
+    t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
