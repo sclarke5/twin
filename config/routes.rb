4592ce5dbe8f788ed831210df237ed8f32fe9433
sclarke5 <sclarke5555@gmail.com>
2018-09-18 18:13:34 -0400

    working model, controller and routes to display reviews on product show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'reviews/edit'
+  get 'reviews/update'
+  get 'reviews/destroy'
+  get 'reviews/index'
+  get 'reviews/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
 
